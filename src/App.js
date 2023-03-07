664af34adea59432dc4502b3c09883612555d1e4
Florian <florianpaulschoene@gmail.com>
2023-03-07 14:49:57 +0100

    init and first steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dice from "./components/Dice";
+
+function allNewDice() {
+	const diceArray = [];
+
+	for (let i = 0; i < 10; i++) {
+		const rndNum = Math.floor(Math.random() * 10);
+	}
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main className="game">
+			<div className="dice--area">
+				<Dice value="1" />
+				<Dice value="1" />
+				<Dice value="1" />
+				<Dice value="1" />
+				<Dice value="1" />
+				<Dice value="1" />
+				<Dice value="1" />
+				<Dice value="1" />
+				<Dice value="1" />
+				<Dice value="1" />
+			</div>
+		</main>
+	);
 }
 
 export default App;
