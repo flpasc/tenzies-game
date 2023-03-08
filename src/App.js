03b046a360fb40b471bf0f7c5c18e1c1ccc81058
Florian <florianpaulschoene@gmail.com>
2023-03-08 10:21:14 +0100

    add hold dice and won mechanics

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import uuid from "react-uuid";
 
 export default function App() {
 	const [diceArray, setDiceArray] = useState(allNewDice());
+	const [tenzies, setTenzies] = useState(false);
+
+	useEffect(() => {
+		tenzies ? console.log("you won") : console.log("");
+	}, [tenzies]);
+
+	useEffect(() => {
+		setTenzies(() => {
+			return diceArray.every((dice) => {
+				return dice.isHeld && dice.value === diceArray[0].value;
+			});
+		});
+	}, [diceArray]);
 
 	const diceElements = diceArray.map((dice) => (
 		<Dice
@@ -18,21 +31,29 @@ export default function App() {
 		/>
 	));
 
+	function generateNewDice() {
+		return {
+			value: Math.ceil(Math.random() * 6),
+			isHeld: false,
+			id: uuid(),
+		};
+	}
+
 	// returns 10 random [numbers]
 	function allNewDice() {
 		const rndDiceArray = [];
 		for (let i = 0; i < 10; i++) {
-			rndDiceArray.push({
-				value: Math.ceil(Math.random() * 6),
-				isHeld: false,
-				id: uuid(),
-			});
+			rndDiceArray.push(generateNewDice());
 		}
 		return rndDiceArray;
 	}
 
 	function rollDice() {
-		setDiceArray(allNewDice());
+		setDiceArray((prevArray) => {
+			return prevArray.map((dice) => {
+				return dice.isHeld ? dice : generateNewDice();
+			});
+		});
 	}
 
 	function holdDice(id) {
@@ -45,6 +66,11 @@ export default function App() {
 
 	return (
 		<main className="game">
+			<h1 className="header">Tenzies</h1>
+			<p className="description">
+				Roll until all dice are the same. Click each die to freeze it at its current calue between
+				rolls.
+			</p>
 			<div className="dice--area">{diceElements} </div>
 			<button className="button-reroll" onClick={rollDice}>
 				Roll
